537bc66cadfb3ac086970b22b52e63ca36f7304b
Panuwit <panuwit_89@hotmail.com>
2025-02-14 20:30:06 +0700

    Latest:Notion17

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,13 +4,15 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
@@ -54,5 +56,10 @@ class User extends Authenticatable
     public function isUser() : bool
     {
         return $this->role === 'USER';
+    }
+
+    public function points() : HasMany
+    {
+        return $this->hasMany(Point::class);
     }
 }
